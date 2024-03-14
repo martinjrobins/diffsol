32d97c3d27278f81069434e5a3958d47dfcb48f2
martinjrobins <martinjrobins@gmail.com>
2024-03-14 10:09:10 +0000

    add new diffsl api

--- src/op/diffsl.rs
+++ src/op/diffsl.rs
@@ -6,7 +6,7 @@ use anyhow::Result;
 
 use crate::OdeEquations;
 
-use super::{NonLinearOp, Op};
+use super::Op;
 
 type T = f64;
 type V = nalgebra::DVector<T>;
@@ -16,6 +16,7 @@ type M = nalgebra::DMatrix<T>;
 struct DiffSl {
     compiler: Compiler,
     data: RefCell<Vec<T>>,
+    ddata: RefCell<Vec<T>>,
     nstates: usize,
     nparams: usize,
 }
@@ -24,12 +25,15 @@ impl DiffSl {
     pub fn new(text: &str, p: V) -> Result<Self> {
         let compiler = Compiler::from_discrete_str(text)?;
         let mut data = compiler.get_new_data();
+        let mut ddata = compiler.get_new_data();
         compiler.set_inputs(p.as_slice(), data.as_mut_slice());
         let data = RefCell::new(data);
+        let ddata = RefCell::new(ddata);
         let (nstates, nparams, _nout, _ndata, _stop) = compiler.get_dims();
         Ok(Self {
             compiler,
             data,
+            ddata,
             nparams, 
             nstates,
         })
@@ -57,31 +61,21 @@ impl OdeEquations for DiffSl {
     }
 
     fn rhs_inplace(&self, t: Self::T, y: &Self::V, rhs_y: &mut Self::V) {
-        todo!()
+        self.compiler.rhs(t, y.as_slice(), self.data.borrow_mut().as_mut_slice(), rhs_y.as_mut_slice());
     }
 
     fn rhs_jac_inplace(&self, t: Self::T, x: &Self::V, v: &Self::V, y: &mut Self::V) {
-        todo!()
+        let mut dummy_rhs = Self::V::zeros(self.nstates());
+        self.compiler.rhs_grad(t, x.as_slice(), v.as_slice(), self.data.borrow_mut().as_mut_slice(), self.ddata.borrow_mut().as_mut_slice(), dummy_rhs.as_mut_slice(), y.as_mut_slice());
     }
 
     fn init(&self, _t: Self::T) -> Self::V {
         let mut ret_y = Self::V::zeros(self.nstates());
-        let mut ret_yp = Self::V::zeros(self.nstates());
-        self.compiler.set_u0(ret_y.as_mut_slice(), ret_yp.as_mut_slice(), self.data.borrow_mut().as_mut_slice());
+        self.compiler.set_u0(ret_y.as_mut_slice(), self.data.borrow_mut().as_mut_slice());
         ret_y
     }
-
-    fn residual_inplace(&self, t: Self::T, y_mass: &Self::V, y_rhs: &Self::V, c: Self::T, res: &mut Self::V) {
-        todo!()
+    fn mass_inplace(&self, t: Self::T, v: &Self::V, y: &mut Self::V) {
+        self.compiler.mass(t, v.as_slice(), self.data.borrow_mut().as_mut_slice(), y.as_mut_slice());
     }
-
-    fn residual_jac_inplace(&self, t: Self::T, y: &Self::V, c: Self::T, v: &Self::V, jac: &mut Self::V) {
-        todo!()
-    }
-
-    fn algebraic_indices(&self) -> <Self::V as crate::vector::Vector>::Index {
-        todo!()
-    }
-
 }
 
