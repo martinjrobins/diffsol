a5df5160ba4b3f2ebc3d594f10f311a2516dee2b
martinjrobins <martinjrobins@gmail.com>
2024-07-03 15:53:15 +0000

    remove examples

--- examples/robertson.rs
+++
@@ -1,9 +0,0 @@
-use diffsol::{ode_solver::test_models::robertson::robertson, Bdf, OdeSolverMethod};
-
-fn main() {
-    let mut s = Bdf::default();
-    let (problem, _soln) = robertson::<nalgebra::DMatrix<f64>>(false);
-    for _ in 0..100000 {
-        let _y = s.solve(&problem, 4.0000e+10);
-    }
-}
