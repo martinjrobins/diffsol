d8e3fb9fb7bd5a8f528d9a8176f135d1a6da03f5
martinjrobins <martinjrobins@gmail.com>
2026-01-13 14:29:36 +0000

    doc: improve Vector

--- diffsol/src/vector/mod.rs
+++ diffsol/src/vector/mod.rs
@@ -17,6 +17,10 @@ pub mod cuda;
 #[macro_use]
 mod utils;
 
+/// A trait for types that represent a collection of indices into a vector.
+///
+/// This is used to represent subsets of vector elements, typically for algebraic constraints
+/// or when operating on specific vector components.
 pub trait VectorIndex: Sized + Debug + Clone {
     type C: Context;
     fn context(&self) -> &Self::C;
@@ -29,6 +33,7 @@ pub trait VectorIndex: Sized + Debug + Clone {
     fn from_vec(v: Vec<IndexType>, ctx: Self::C) -> Self;
 }
 
+/// Common interface for vector-like types, providing access to scalar type, context, and inner representation.
 pub trait VectorCommon: Sized + Debug {
     type T: Scalar;
     type C: Context;
@@ -61,6 +66,9 @@ where
     }
 }
 
+/// Operations on vectors by value (addition and subtraction).
+///
+/// This trait defines vector addition and subtraction when both operands are owned or references.
 pub trait VectorOpsByValue<Rhs = Self, Output = Self>:
     VectorCommon + Add<Rhs, Output = Output> + Sub<Rhs, Output = Output>
 {
@@ -71,10 +79,17 @@ impl<V, Rhs, Output> VectorOpsByValue<Rhs, Output> for V where
 {
 }
 
+/// In-place operations on vectors (addition and subtraction).
+///
+/// This trait defines in-place vector addition and subtraction (self += rhs, self -= rhs).
 pub trait VectorMutOpsByValue<Rhs = Self>: VectorCommon + AddAssign<Rhs> + SubAssign<Rhs> {}
 
 impl<V, Rhs> VectorMutOpsByValue<Rhs> for V where V: VectorCommon + AddAssign<Rhs> + SubAssign<Rhs> {}
 
+/// Operations on a reference to a vector, supporting addition, subtraction, and scalar multiplication.
+///
+/// This trait ensures that vector references can be used in arithmetic operations with owned vectors,
+/// other references, and vector views, enabling flexible composition of vector operations.
 pub trait VectorRef<V: Vector>:
     VectorOpsByValue<V, V>
     + for<'a> VectorOpsByValue<&'a V, V>
@@ -93,6 +108,11 @@ impl<RefT, V: Vector> VectorRef<V> for RefT where
 {
 }
 
+/// A mutable view into a vector, supporting in-place operations and modifications.
+///
+/// This trait represents a temporary mutable reference to a vector's data, allowing in-place
+/// arithmetic operations (+=, -=, *=) and other modifications. Mutable views can be created
+/// via the `as_view_mut()` method on a `Vector`.
 pub trait VectorViewMut<'a>:
     VectorMutOpsByValue<Self::View>
     + VectorMutOpsByValue<Self::Owned>
@@ -103,11 +123,19 @@ pub trait VectorViewMut<'a>:
     type Owned;
     type View;
     type Index: VectorIndex;
+    /// Copy values from an owned vector into this view.
     fn copy_from(&mut self, other: &Self::Owned);
+    /// Copy values from another vector view into this view.
     fn copy_from_view(&mut self, other: &Self::View);
+    /// Compute the AXPY operation: self = alpha * x + beta * self
     fn axpy(&mut self, alpha: Self::T, x: &Self::Owned, beta: Self::T);
 }
 
+/// A borrowed immutable view of a vector, supporting read-only arithmetic operations.
+///
+/// This trait represents a temporary immutable reference to a vector's data, allowing read-only
+/// operations like addition, subtraction, and scalar multiplication. Vector views can be created
+/// via the `as_view()` method on a `Vector` and are cheaper to create than cloning.
 pub trait VectorView<'a>:
     VectorOpsByValue<Self, Self::Owned>
     + VectorOpsByValue<Self::Owned, Self::Owned>
@@ -116,10 +144,19 @@ pub trait VectorView<'a>:
     + Mul<Scale<Self::T>, Output = Self::Owned>
 {
     type Owned;
+    /// Compute the squared weighted norm: sum_i ((self_i) / (|y_i| * rtol + atol_i))^2
+    ///
+    /// This is commonly used for error control in ODE solvers.
     fn squared_norm(&self, y: &Self::Owned, atol: &Self::Owned, rtol: Self::T) -> Self::T;
+    /// Convert this view into an owned vector, cloning the data if necessary.
     fn into_owned(self) -> Self::Owned;
 }
 
+/// A complete vector abstraction supporting arithmetic operations, norms, and index operations.
+///
+/// This is the main vector trait used throughout diffsol. Implementing vectors can be hosted on CPU (see `VectorHost`) or GPU.
+/// Users typically do not need to implement this trait; use provided implementations like
+/// `NalgebraVec` or `FaerVec`.
 pub trait Vector:
     VectorOpsByValue<Self>
     + for<'b> VectorOpsByValue<&'b Self>
@@ -142,110 +179,110 @@ pub trait Vector:
         Self: 'a;
     type Index: VectorIndex;
 
-    /// get the context
+    /// Get the context associated with this vector (for device placement, threading, etc.).
     fn context(&self) -> &Self::C;
 
+    /// Get a mutable reference to the inner representation of the vector.
     fn inner_mut(&mut self) -> &mut Self::Inner;
 
-    /// set the value at index `index` to `value`, might be slower than using `IndexMut`
-    /// if the vector is not on the host
+    /// Set the value at the specified index to `value`.
     fn set_index(&mut self, index: IndexType, value: Self::T);
 
-    /// get the value at index `index`, might be slower than using `Index`
-    /// if the vector is not on the host
+    /// Get the value at the specified index.
     fn get_index(&self, index: IndexType) -> Self::T;
 
-    /// returns (\sum_i (x_i)^k)^(1/k)
+    /// Compute the $\ell_k$ norm: $(\sum_i |x_i|^k)^{1/k}$
     fn norm(&self, k: i32) -> Self::T;
 
-    /// returns \sum_i (x_i / (|y_i| * rtol + atol_i))^2
+    /// Compute the squared weighted norm for error control: $\sum_i (x_i / (|y_i| \cdot \text{rtol} + \text{atol}_i))^2$
+    ///
+    /// This norm is used by ODE solvers for adaptive error control.
     fn squared_norm(&self, y: &Self, atol: &Self, rtol: Self::T) -> Self::T;
 
-    /// get the length of the vector
+    /// Get the length (number of elements) in this vector.
     fn len(&self) -> IndexType;
 
-    /// check if the vector is empty
+    /// Check if the vector is empty.
     fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
-    /// create a vector of size `nstates` with all elements set to `value`
+    /// Create a vector of length `nstates` with all elements initialized to `value`.
     fn from_element(nstates: usize, value: Self::T, ctx: Self::C) -> Self;
 
-    /// create a vector of size `nstates` with all elements set to zero
+    /// Create a vector of length `nstates` with all elements set to zero.
     fn zeros(nstates: usize, ctx: Self::C) -> Self {
         Self::from_element(nstates, Self::T::zero(), ctx)
     }
 
-    /// fill the vector with `value`
+    /// Fill all elements of this vector with `value`.
     fn fill(&mut self, value: Self::T);
 
-    /// create a view of the vector
+    /// Create an immutable view of this vector.
     fn as_view(&self) -> Self::View<'_>;
 
-    /// create a mutable view of the vector
+    /// Create a mutable view of this vector.
     fn as_view_mut(&mut self) -> Self::ViewMut<'_>;
 
-    /// copy the values from `other` to `self`
+    /// Copy all values from `other` into this vector.
     fn copy_from(&mut self, other: &Self);
 
-    /// copy the values from `other` to `self`
+    /// Copy all values from a vector view into this vector.
     fn copy_from_view(&mut self, other: &Self::View<'_>);
 
-    /// create a vector from a Vec
-    /// TODO: would prefer to use From trait but not implemented for faer::Col
+    // TODO: would prefer to use From trait but not implemented for faer::Col
+    /// Create a vector from a Rust `Vec`.
     fn from_vec(vec: Vec<Self::T>, ctx: Self::C) -> Self;
 
-    /// create a vector from a slice
+    /// Create a vector from a slice.
     fn from_slice(slice: &[Self::T], ctx: Self::C) -> Self;
 
-    /// convert the vector to a Vec
-    /// TODO: would prefer to use Into trait but not implemented for faer::Col
+    // TODO: would prefer to use From trait but not implemented for faer::Col
+    /// Clone this vector as a Rust `Vec`.
     fn clone_as_vec(&self) -> Vec<Self::T>;
 
-    /// axpy operation: self = alpha * x + beta * self
+    /// Compute the AXPY operation: self = alpha * x + beta * self
     fn axpy(&mut self, alpha: Self::T, x: &Self, beta: Self::T);
 
-    /// axpy operation: self = alpha * x + beta * self
+    /// Compute the AXPY operation with a vector view: self = alpha * x + beta * self
     fn axpy_v(&mut self, alpha: Self::T, x: &Self::View<'_>, beta: Self::T);
 
-    /// element-wise multiplication
+    /// Element-wise multiplication: self_i *= other_i
     fn component_mul_assign(&mut self, other: &Self);
 
-    /// element-wise division
+    /// Element-wise division: self_i /= other_i
     fn component_div_assign(&mut self, other: &Self);
 
-    /// given two vectors `g0=self` and `g1`, return:
-    /// - `true` if a root is found in g1 (i.e. g1_i == 0)
-    /// - for all values of i where a zero crossing is found (i.e. g0_i * g1_i < 0), return:
-    ///     - max_i(abs(g1_i / (g1_i - g0_i))), 0 otherwise
-    ///     - the index i at the maximum value, -1 otherwise
+    /// Detect roots (zero crossings) between this vector (as g0) and another vector (g1).
+    ///
+    /// Returns a tuple of:
+    /// - `bool`: true if a zero crossing is found (g1_i == 0 for some i)
+    /// - `T`: the interpolation factor at the maximum crossing (0 if none found) (given by maxmimum |g0_i / (g1_i - g0_i)|)
+    /// - `i32`: the index of the maximum crossing (-1 if none found)
     fn root_finding(&self, g1: &Self) -> (bool, Self::T, i32);
 
-    /// assign `value` to the elements of `self` at the indices specified by `indices`
-    /// i.e. `self[indices[i]] = value` for all i
+    /// Assign `value` to all elements at the specified indices.
     fn assign_at_indices(&mut self, indices: &Self::Index, value: Self::T);
 
-    /// copy from `other` at the indices specified by `indices`
-    /// generaly `self` and `other` have the same length
-    /// i.e. `self[indices[i]] = other[indices[i]]` for all i
+    /// Copy values from `other` at the specified indices: self[indices[i]] = other[indices[i]]
     fn copy_from_indices(&mut self, other: &Self, indices: &Self::Index);
 
-    /// gather values from `other` at the indices specified by `indices`
-    /// i.e. `self[i] = other[indices[i]]` for all i
+    /// Gather values from `other` at indices: self[i] = other[indices[i]]
     fn gather(&mut self, other: &Self, indices: &Self::Index);
 
-    /// scatter values from `self` to `other` at the indices specified by `indices`
-    /// i.e. `other[indices[i]] = self[i]` for all i
+    /// Scatter values to `other` at indices: other[indices[i]] = self[i]
     fn scatter(&self, indices: &Self::Index, other: &mut Self);
 
-    /// assert that `self` is equal to `other` within a tolerance `tol`
+    /// Assert that this vector equals `other` within a scalar tolerance `tol`.
     fn assert_eq_st(&self, other: &Self, tol: Self::T) {
         let tol = vec![tol; self.len()];
         Self::assert_eq_vec(self.clone_as_vec(), other.clone_as_vec(), tol);
     }
 
-    /// assert that `self` is equal to `other` using the same norm used by the solvers
+    /// Assert that this vector equals `other` using a weighted norm (same as used by ODE solvers).
+    ///
+    /// Uses `squared_norm` internally with the scaling factors, and asserts that the resulting
+    /// norm is less than `factor`.
     fn assert_eq_norm(&self, other: &Self, atol: &Self, rtol: Self::T, factor: Self::T) {
         let error = self.clone() - other.clone();
         let error_norm = error.squared_norm(other, atol, rtol).sqrt();
@@ -255,7 +292,7 @@ pub trait Vector:
         );
     }
 
-    /// assert that `self` is equal to `other` within a vector tolerance `tol`
+    /// Assert that this vector equals `other` using a vector of per-element tolerances.
     fn assert_eq(&self, other: &Self, tol: &Self) {
         assert_eq!(
             self.len(),
@@ -315,18 +352,25 @@ pub trait Vector:
     }
 }
 
+/// A vector hosted on the CPU, supporting direct indexing and slice access.
+///
+/// This trait extends `Vector` with the ability to directly access vector elements via indexing
+/// (`v[i]`) and to get slices of the underlying data. This is useful for algorithms that need
+/// direct CPU-side access to vector data. GPU vectors typically do not implement this trait.
 pub trait VectorHost:
     Vector + Index<IndexType, Output = Self::T> + IndexMut<IndexType, Output = Self::T>
 {
-    /// get the vector as a slice
-    /// TODO: not compatible with gpu vectors? but used for diffsl api
+    /// Get the vector data as an immutable slice.
     fn as_slice(&self) -> &[Self::T];
 
-    /// get the vector as a mut slice
-    /// TODO: not compatible with gpu vectors? but used for diffsl api
+    /// Get the vector data as a mutable slice.
     fn as_mut_slice(&mut self) -> &mut [Self::T];
 }
 
+/// Marker trait for vectors that have a default associated dense matrix type.
+///
+/// This trait associates a vector type with its corresponding dense matrix representation,
+/// enabling vectors to be easily combined with matrix types for linear algebra operations.
 pub trait DefaultDenseMatrix: Vector {
     type M: DenseMatrix<V = Self, T = Self::T, C = Self::C>;
 }
