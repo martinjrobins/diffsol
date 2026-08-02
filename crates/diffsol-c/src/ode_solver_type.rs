cb377b25ab2461423ab41d8db8b39fe6e237928b
martinjrobins <martinjrobins@gmail.com>
2026-08-02 14:41:30 +0000

    fix: add use Vector

--- crates/diffsol-c/src/ode_solver_type.rs
+++ crates/diffsol-c/src/ode_solver_type.rs
@@ -739,7 +739,7 @@ mod tests {
         OdeSolverProblem,
     };
     #[cfg(feature = "diffsl-llvm")]
-    use diffsol::{DefaultDenseMatrix, MatrixCommon};
+    use diffsol::{DefaultDenseMatrix, MatrixCommon, Vector};
 
     #[cfg(feature = "diffsl-llvm")]
     use crate::linear_solver_type::LinearSolverType;
